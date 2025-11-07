3f273556a02132400f78cd270d5014ef2f9abfd5
mhahnsjoe <56585182+mhahnsjoe@users.noreply.github.com>
2025-11-07 11:17:28 +0100

    Initial project setup - backend + frontend scaffolds

---
+++ frontend/src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const API_BASE_URL = "http://localhost:8080";
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
