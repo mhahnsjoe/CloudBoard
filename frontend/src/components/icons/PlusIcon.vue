90a076bb326d30690e5f725a92aeab4d1d40b4bb
mhahnsjoe <56585182+mhahnsjoe@users.noreply.github.com>
2025-11-10 21:26:29 +0100

    Added Icon components

---
+++ frontend/src/components/icons/PlusIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg :class="className" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PlusIcon',
+  props: {
+    className: {
+      type: String,
+      default: 'w-5 h-5'
+    }
+  }
+});
+</script>
